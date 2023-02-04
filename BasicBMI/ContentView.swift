7b6ab0b28dea874b883b89d2c9560732597ba817
Pasindu Akalpa <pasinduakalpa1998@gmail.com>
2023-02-04 17:12:44 +0530

    Add Weight Category Logic

--- BasicBMI/ContentView.swift
+++ BasicBMI/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var weightText: String = ""
     @State private var heightText: String = ""
     @State private var bmi: Double = 0
+    @State private var classificationText: String = ""
     
     var body: some View {
         VStack {
@@ -26,13 +27,23 @@ struct ContentView: View {
                 let weight = Double(self.weightText) ?? 0.0
                 let height = Double(self.heightText) ?? 0.0
                 self.bmi = weight/(height * height)
+                
+                if (self.bmi < 18.5) {
+                    self.classificationText = "Underweight"
+                } else if (self.bmi < 24.9) {
+                    self.classificationText = "Healthy weight"
+                } else if (self.bmi < 29.9) {
+                    self.classificationText = "Overweight"
+                } else {
+                    self.classificationText = "Obese"
+                }
             } label: {
                 Text("Calculate BMI")
                     .padding()
                     .foregroundColor(Color.white)
                     .background(Color.blue)
             }
-            Text("BMI: \(bmi)")
+            Text("BMI: \(bmi, specifier: "%.1f"), \(classificationText)")
                 .font(.title)
                 .padding()
         }.padding()
